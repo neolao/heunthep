58215413db1aee9f246473223448baf47cb29844
neolao <contact@neolao.com>
2015-06-26 11:03:12 +0200

    AWESOME COMMIT

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-fs.readFile('./image.txt', 'utf8', function(error, content) {
+fs.readFile(__dirname + '/image.txt', 'utf8', function(error, content) {
     console.log(content);
 });
